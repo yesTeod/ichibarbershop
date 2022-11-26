85334519e03abe5db46e3318aba29fb3139d65b1
tszhh <101996502+tszhh@users.noreply.github.com>
2022-11-26 12:34:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
-app.use(express.static('build'));
+
+//app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //API 
 app.use("/api/services", serviceRouter);
