4fbadd8a496f3cb75cf605f4b691ebd500631384
tszhh <101996502+tszhh@users.noreply.github.com>
2022-11-26 12:39:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ connectDatabase();
 const app = express();
 app.use(express.json());
 
-//app.use(express.static('build'));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static('build'));
 
 //API 
 app.use("/api/services", serviceRouter);
